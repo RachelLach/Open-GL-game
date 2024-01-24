57301f2700b917494c3dd835a7664c573e982e75
RachelLach <144023790+RachelLach@users.noreply.github.com>
2024-01-24 11:33:37 +0100

    Update: I finally did double triangle

--- Open GL game/Open GL game.cpp
+++ Open GL game/Open GL game.cpp
@@ -1,20 +1,155 @@
-// Open GL game.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
 
-int main()
+using namespace std;
+
+void processInput(GLFWwindow*);
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
-    std::cout << "Hello World!\n";
+    glViewport(0, 0, width, height);
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void error_callback(int error, const char* msg) {
+    cerr << " [" << error << "] " << msg << endl;
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+int main() {
+
+    glfwSetErrorCallback(error_callback);
+
+    // Initialize GLFW
+    if (!glfwInit()) { // Exit, if it failed
+        cout << "Failed to init GLFW" << endl;
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#if __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    // Request Window from Operating System
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+    if (window == nullptr)
+    {
+        cout << "Failed to create GLFW window" << endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    // Initialize GLAD (connects OpenGL Functions)
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        cout << "Failed to initialize GLAD" << endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    glViewport(0, 0, width, height);
+
+    // Initialization ends here
+    // ==================================================================
+    // The Real Program starts here
+    float red = 0;
+
+    // ----- Create Vertex Array Object, which makes changing between VBOs easier -----
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    // ----- Create Array Buffer on the GPU and copy our vertices to GPU -------
+    float vertices[] = {
+       -0.75f, -0.75f, 0.0f,
+        0.25f, -0.75f, 0.0f,
+       -0.25f,  0.25f, 0.0f,
+
+        0.25f, -0.75f, 0.0f,
+        0.75f, -0.75f, 0.0f,
+        0.50f,  0.50f, 0.0f
+    };
+
+    unsigned int VBO; // variable to store buffer id
+    glGenBuffers(1, &VBO); // ask open gl to create a buffer
+    glBindBuffer(GL_ARRAY_BUFFER, VBO); // tell gl to use this buffer
+    glBufferData(GL_ARRAY_BUFFER, // copy our vertices to the buffer
+        sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // ----- Compile the Vertex Shader on the GPU -------
+
+    const char* vertexShaderSource = "#version 330 core\n"
+        "layout (location = 0) in vec3 aPos;\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+        "}\0";
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    // ------ Compile the Fragment Shader on the GPU --------
+    const char* fragmentShaderSource = "#version 330 core\n"
+        "out vec4 FragColor;\n"
+        "void main()\n"
+        "{\n"
+        "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+        "} \0";
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    // -------- Create Shader Program (Render Pipeline) ---------
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    glUseProgram(shaderProgram);
+    // clean up shaders after they've been linked into a program
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    // ------ configure vertex attribute(s) (currently it's just one, the position) -----
+    // so the vertex shader understands, where to find the input attributes, in this case position
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // While the User doesn't want to Quit (X Button, Alt+F4)
+    while (!glfwWindowShouldClose(window))
+    {
+        // process input (e.g. close window on Esc)
+        glfwPollEvents();
+        processInput(window);
+        red += 0.00001f;
+        if (red > 1)
+            red -= 1;
+
+        // render (paint the current frame of the game)
+        glClearColor(red, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+
+
+        // present (send the current frame to the computer screen)
+        glfwSwapBuffers(window); // ??
+    }
+    // Cleans up all the GLFW stuff
+    glfwTerminate();
+    return 0;
+}
+
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
